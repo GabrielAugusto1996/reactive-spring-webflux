958baa6319ce94419346b97a7606c39dbf05786f
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-11-07 16:18:44 +0100

    Let's write our very first Flux

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+
+        return Flux.fromIterable(List.of("Alex", "Ben", "Cloe"));
+    }
+
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(System.out::println);
+    }
 }
