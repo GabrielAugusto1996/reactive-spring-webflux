4679d78ce688de142db84670a130721b3adc60dc
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-11-07 16:52:17 +0100

    Testing Flux using JUnit5 + StepVerifier

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,41 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    private final FluxAndMonoGeneratorService fluxAndMonoGeneratorService;
+
+    FluxAndMonoGeneratorServiceTest() {
+        fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+    }
+
+    @Test
+    void namesFlux_FluxString_WhenSuccess() {
+        //given
+        //when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("Alex", "Ben", "Cloe")
+                //.expectNextCount(3) You can use it to verify if you flux have X elements
+                .verifyComplete();
+
+    }
+
+    @Test
+    void nameMono_MonoString_WhenSuccess() {
+        //given
+        //when
+        var nameMono = fluxAndMonoGeneratorService.nameMono();
+
+        //then
+        StepVerifier.create(nameMono)
+                .expectNext("John")
+                .verifyComplete();
+
+    }
 }
