c2890216775ae73b929a4ced3d02d9c15446ddba
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-11-07 16:21:30 +0100

    Let's write our very first Mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -11,11 +12,22 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("Alex", "Ben", "Cloe"));
     }
 
+    public Mono<String> nameMono() {
+
+        return Mono.just("John");
+    }
+
     public static void main(String[] args) {
 
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
+
+        //Flux
         fluxAndMonoGeneratorService.namesFlux()
                 .subscribe(System.out::println);
+
+        //Mono
+
+        fluxAndMonoGeneratorService.nameMono().subscribe(System.out::println);
     }
 }
