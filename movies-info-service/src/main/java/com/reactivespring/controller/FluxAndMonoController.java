581df7be981e2e86f900f6974ec9abfbb16d7bbd
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-11-10 17:34:21 +0100

    Build an infinitive API

--- movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
@@ -1,11 +1,13 @@
 package com.reactivespring.controller;
 
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Random;
 
 @RestController
@@ -19,5 +21,11 @@ public class FluxAndMonoController {
     @GetMapping("mono")
     public Mono<Integer> mono() {
         return Mono.just(new Random().nextInt()).log();
+    }
+
+    //When you are working with stream, you must inform the produce type.
+    @GetMapping(value = "stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream() {
+        return Flux.interval(Duration.ofSeconds(1)).log();
     }
 }
