f5fe140cba95c3fb2519c4c8d65ad6daf4929f00
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-11-13 19:28:41 +0100

    Unit Test Cases for the Reviews Service in the ReviewsUnitTest class

--- movies-review-service/src/test/java/com/reactivespring/router/ReviewUnitTest.java
+++ movies-review-service/src/test/java/com/reactivespring/router/ReviewUnitTest.java
@@ -11,10 +11,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.net.URI;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
@@ -54,6 +58,106 @@ public class ReviewUnitTest {
                     assertNotNull(responseBody);
                     assertNotNull(responseBody.getReviewId());
                 });
+    }
+
+    @Test
+    void findAll_Review_WhenSuccess() {
+        final Review review = new Review();
+        review.setReviewId("1");
+        review.setComment("This movie is good");
+        review.setRating(10.0);
+        review.setMovieInfoId(1L);
+
+        when(reviewReactiveRepository.findAll()).thenReturn(Flux.fromIterable(List.of(review)));
+
+        webTestClient
+                .get()
+                .uri(URI.create(BASE_URI))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Review.class)
+                .hasSize(1);
+    }
+
+    @Test
+    void findAllByMovieInfoId_Review_WhenSuccess() {
+        final Review review = new Review();
+        review.setReviewId("1");
+        review.setComment("This movie is good");
+        review.setRating(10.0);
+        review.setMovieInfoId(1L);
+
+        when(reviewReactiveRepository.findAllByMovieInfoId(1L)).thenReturn(Flux.fromIterable(List.of(review)));
+
+        webTestClient
+                .get()
+                .uri(URI.create(BASE_URI + "?moviesinfo=1"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Review.class)
+                .hasSize(1);
+    }
+
+    @Test
+    void updateReview_Review_WhenSuccess() {
+        final Review review = new Review();
+        review.setComment("This movie is good");
+        review.setRating(10.0);
+        review.setMovieInfoId(1L);
+        review.setReviewId("1");
+
+        when(reviewReactiveRepository.findById("1")).thenReturn(Mono.just(review));
+        when(reviewReactiveRepository.save(review)).thenReturn(Mono.just(review));
+
+
+        webTestClient
+                .put()
+                .uri(URI.create(BASE_URI + "/" + "1"))
+                .bodyValue(review)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Review.class)
+                .consumeWith(result -> {
+                    Review responseBody = result.getResponseBody();
+
+                    assertNotNull(responseBody);
+                    assertNotNull(responseBody.getReviewId());
+                    assertEquals("This movie is good", responseBody.getComment());
+                });
+    }
+
+    @Test
+    void updateReview_Review_WhenNotFound() {
+        final Review review = new Review();
+        review.setComment("This movie is good");
+        review.setRating(10.0);
+        review.setMovieInfoId(1L);
+
+        when(reviewReactiveRepository.findById("2")).thenReturn(Mono.empty());
+
+        webTestClient
+                .put()
+                .uri(URI.create(BASE_URI + "/" + "2"))
+                .bodyValue(review)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void deleteReview_Review_WhenSuccess() {
+        when(reviewReactiveRepository.deleteById("1")).thenReturn(Mono.empty());
+
+
+        webTestClient
+                .delete()
+                .uri(URI.create(BASE_URI + "/" + "1"))
+                .exchange()
+                .expectStatus().isNoContent();
+
+        Flux<Review> reviews = reviewReactiveRepository.findAll();
+
+        StepVerifier.create(reviews)
+                .expectNextCount(0L);
     }
 
 }
