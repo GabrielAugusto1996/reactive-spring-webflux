b7de4ed2d58a70fb5665146cd90d6b62eb089c83
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-11-14 16:53:12 +0100

    Stubs for MoviesInfoService and ReviewService

--- movies-service/src/test/java/com/reactivespring/controller/MoviesControllerIntTest.java
+++ movies-service/src/test/java/com/reactivespring/controller/MoviesControllerIntTest.java
@@ -1,20 +1,62 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.domain.Movie;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = "test")
 @AutoConfigureWebTestClient
 @AutoConfigureWireMock(port = 8084)
 @TestPropertySource(properties = {
-        "restClient.movieInfoUrl=http://localhost:8084/v1/movieinfos",
-        "restClient.reviewUrl=http://localhost:8084/v1/reviews"
+        "restClient.movieInfoUrl=http://localhost:8084",
+        "restClient.reviewUrl=http://localhost:8084"
 })
 class MoviesControllerIntTest {
 
+    @Autowired
+    WebTestClient webTestClient;
 
+
+    @Test
+    void retrieveMovieById_Movie_WhenSuccess() {
+        //given
+        var movieId = "1";
+
+        stubFor(get(urlEqualTo("/v1/movieinfos/" + movieId))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("movieinfo.json")));
+
+        stubFor(get(urlEqualTo("/v1/reviews?moviesinfo=" + movieId))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("reviews.json")));
+
+        //when
+        webTestClient.
+                get()
+                .uri("/v1/movies/{id}", movieId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Movie.class)
+                .consumeWith(movieEntityExchangeResult -> {
+                    var movie = movieEntityExchangeResult.getResponseBody();
+
+                    Assertions.assertNotNull(movie);
+                    Assertions.assertEquals(2, movie.getReviewList().size());
+                });
+    }
 }
