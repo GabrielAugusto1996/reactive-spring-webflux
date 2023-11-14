8f121b2670e15a7fcf3e8403ef3bffce90c251b2
Gabriel Augusto <gabriel.morato139@gmail.com>
2023-11-14 15:55:14 +0100

    Combine the MoviesInfoRestClient and ReviewRestClient

--- movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -1,5 +1,7 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.client.MovieInfoRestClient;
+import com.reactivespring.client.ReviewRestClient;
 import com.reactivespring.domain.Movie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,23 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/v1/movies")
 public class MoviesController {
 
+    private final MovieInfoRestClient movieInfoRestClient;
+    private final ReviewRestClient reviewRestClient;
+
+    public MoviesController(MovieInfoRestClient movieInfoRestClient, ReviewRestClient reviewRestClient) {
+        this.movieInfoRestClient = movieInfoRestClient;
+        this.reviewRestClient = reviewRestClient;
+    }
+
     @GetMapping("/{id}")
     public Mono<Movie> retrieveMovieById(@PathVariable("id") String movieId) {
 
-        return null;
+        return movieInfoRestClient
+                .findMovieInfoById(movieId)
+                .flatMap(movieInfo -> {
+                    var reviewMonoList = reviewRestClient.findAllReviewByMovieInfoId(movieId).collectList();
+
+                    return reviewMonoList.map(reviews -> new Movie(movieInfo, reviews));
+                });
     }
 }
